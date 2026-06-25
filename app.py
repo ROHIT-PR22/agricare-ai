edb78562c5c93324d5dce5a92a843f3a1b7e7aea
Rohit Prajapati <rohit2004prajapati21@gmail.com>
2026-06-25 22:58:21 +0530

    fix: configure flask for public access

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def predict():
                              image_url=filepath)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
